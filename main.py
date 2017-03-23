d7300654a505f9b20f1fa1c91b263aaf1712ab8b
Eric Ball <ericlawsonball@gmail.com>
2017-03-23 01:13:17 -0400

    added tornado files

---
+++ main.py
@@ -0,0 +1,30 @@
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import os
+
+# from tornado.options import define, options
+# define("port", default=8000, help="Which port to use", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            # debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    # http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ readme.md
@@ -0,0 +1,7 @@
+# gumbysguide
+
+## Readme
+1. This is a test
+3. This is also a test.
+
+
