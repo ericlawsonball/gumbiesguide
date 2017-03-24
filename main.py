6558c89d10c0f11f976ea4ab7f8f2692c0f9dd72
Eric Ball <ericlawsonball@gmail.com>
2017-03-23 22:41:57 -0400

    Changed to Hello World

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
-import tornado.web
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import os
+    import os
+    import tornado.httpserver
+    import tornado.ioloop
+    import tornado.web
 
-# from tornado.options import define, options
-# define("port", default=8000, help="Which port to use", type=int)
+    class MainHandler(tornado.web.RequestHandler):
+        def get(self):
+            self.write("Hello world")
 
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
+    def main():
+        application = tornado.web.Application([
             (r"/", MainHandler),
-        ]
-        settings = dict(
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
-            # debug=True,
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
+        ])
+        http_server = tornado.httpserver.HTTPServer(application)
+        port = int(os.environ.get("PORT", 5000))
+        http_server.listen(port)
+        tornado.ioloop.IOLoop.instance().start()
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html")
-
-if __name__ == "__main__":
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
-    # http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    if __name__ == "__main__":
+        main()
