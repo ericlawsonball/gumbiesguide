370a7542980ce6c80f5d7168702a4a20d505bea0
Eric Ball <ericlawsonball@gmail.com>
2017-03-28 20:46:30 -0400

    fixed things

--- handlers/index.py
+++ handlers/index.py
@@ -4,6 +4,5 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
 
         records=self.application.db.query("SELECT * FROM mpg")
-
         self.render("index.html", records=records)
         # self.write(":)")
